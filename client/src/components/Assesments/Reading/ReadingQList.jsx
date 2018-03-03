0a52728fa0ce5a7fd9c46c6db81808e727703165
miriam30 <miriam.alvarez.p96@gmail.com>
2018-03-03 04:52:43 -0600

    reading assesment rendering

--- client/src/components/Assesments/Reading/ReadingQList.jsx
+++ client/src/components/Assesments/Reading/ReadingQList.jsx
@@ -60,6 +60,15 @@ system currently in place.</p>
         <ReadingQItem reading={reading}
         key={reading.readingID}></ReadingQItem>
       )}
+
+      Based on the information in the passage, explain SSO in your own words. What does it stand for? What does it do? What are its advantages and disadvantages? What are some alternatives to SSO?
+      <textarea rows="8" cols="100"></textarea>
+      <br />
+      Of the password solutions referenced in the passage, which sounds most appealing to you? How do you currently keep track of all of your passwords? Do you think you should switch to another method? Why or why not?
+      <textarea rows="8" cols="100"></textarea>
+      <br />
+      Make some inferences about the author. What do you think is his or her academic or professional background? What type of audience do you think he or she is trying to reach? Which password solution do you think he or she most prefers? Why?
+      <textarea rows="8" cols="100"></textarea>
   </div>
 )
 export default ReadingTest;
